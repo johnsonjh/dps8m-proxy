fa22433e9946279b691ea2762a556479e3ed1d59
Jeffrey H. Johnson <trnsz@pobox.com>
2025-07-16 09:10:22 -0400

    Remove unnecessary assignment
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

--- main.go
+++ main.go
@@ -835,7 +835,6 @@ func listConfiguration() {
 	if consoleLog != "" {
 		logPath := getConsoleLogPath(time.Now())
 		logPath = filepath.Clean(logPath)
-		quietMode := ""
 		if strings.ToLower(consoleLog) == "quiet" {
 			quietMode = "\r\n* Console Logging Mode: Quiet"
 		} else {
