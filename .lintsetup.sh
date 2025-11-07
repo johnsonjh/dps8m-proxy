35e7d454e22c589e3d11376c1ac3c49392e5a201
Jeffrey H. Johnson <trnsz@pobox.com>
2025-11-07 12:47:23 -0500

    Install gotags and gogtags in .lintsetup.sh
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

--- .lintsetup.sh
+++ .lintsetup.sh
@@ -46,30 +46,43 @@ env printf '%s\n' " to '${GOEXE:?}'..."
 set -x
 
 ###############################################################################
+# Install tag generators (always @master)
+
+env CGO_ENABLED=1 \
+  CGO_CFLAGS="-Dpread64=pread -Dpwrite64=pwrite -Doff64_t=off_t" \
+  "${GO:?}" install -v "github.com/jstemmer/gotags@master" \
+  || env CGO_ENABLED=0 "${GO:?}" install -v "github.com/jstemmer/gotags@master"
+
+env CGO_ENABLED=1 \
+  CGO_CFLAGS="-Dpread64=pread -Dpwrite64=pwrite -Doff64_t=off_t" \
+  "${GO:?}" install -v "github.com/juntaki/gogtags@master" \
+  || env CGO_ENABLED=0 "${GO:?}" install -v "github.com/juntaki/gogtags@master"
+
+###############################################################################
 # Install individual linters
 
-${GO:?} install -v "github.com/boyter/scc/v3@${BRANCH:-}"
-${GO:?} install -v "github.com/kisielk/errcheck@${BRANCH:-}"
-${GO:?} install -v "github.com/mgechev/revive@${BRANCH:-}"
-${GO:?} install -v "golang.org/x/tools/cmd/deadcode@${BRANCH:?}"
-${GO:?} install -v "golang.org/x/vuln/cmd/govulncheck@${BRANCH:-}"
-${GO:?} install -v "honnef.co/go/tools/cmd/staticcheck@${BRANCH:-}"
-${GO:?} install -v "mvdan.cc/gofumpt@${BRANCH:-}"
-${GO:?} install -v "mvdan.cc/sh/v3/cmd/shfmt@${BRANCH:-}"
+"${GO:?}" install -v "github.com/boyter/scc/v3@${BRANCH:-}"
+"${GO:?}" install -v "github.com/kisielk/errcheck@${BRANCH:-}"
+"${GO:?}" install -v "github.com/mgechev/revive@${BRANCH:-}"
+"${GO:?}" install -v "golang.org/x/tools/cmd/deadcode@${BRANCH:?}"
+"${GO:?}" install -v "golang.org/x/vuln/cmd/govulncheck@${BRANCH:-}"
+"${GO:?}" install -v "honnef.co/go/tools/cmd/staticcheck@${BRANCH:-}"
+"${GO:?}" install -v "mvdan.cc/gofumpt@${BRANCH:-}"
+"${GO:?}" install -v "mvdan.cc/sh/v3/cmd/shfmt@${BRANCH:-}"
 
 ###############################################################################
 # Install gopls (always @latest)
 
-${GO:?} install -v "golang.org/x/tools/gopls@latest"
+"${GO:?}" install -v "golang.org/x/tools/gopls@latest"
 
 ###############################################################################
 # Install nilaway and golangci-lint
 
 if [ "${BRANCH:-}" = "master" ]; then
-  go install -v "go.uber.org/nilaway/cmd/nilaway@main"
-  go install -v "github.com/golangci/golangci-lint/v2/cmd/golangci-lint@main"
+  "${GO:?}" install -v "go.uber.org/nilaway/cmd/nilaway@main"
+  "${GO:?}" install -v "github.com/golangci/golangci-lint/v2/cmd/golangci-lint@main"
 else
-  go install -v "go.uber.org/nilaway/cmd/nilaway@latest"
+  "${GO:?}" install -v "go.uber.org/nilaway/cmd/nilaway@latest"
   # shellcheck disable=SC2312
   curl -fsSL \
     "https://raw.githubusercontent.com/golangci/golangci-lint/HEAD/install.sh" \
